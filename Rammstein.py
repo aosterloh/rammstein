686e7be8e4dc096d2049c1ea820cf8717facb488
aosterloh <aosterloh@google.com>
2018-12-24 09:04:52 +0100

    Update Rammstein.py

--- Rammstein.py
+++ Rammstein.py
@@ -1,27 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 from twilio.rest import Client
+import time
 
-account_sid = 'AC1304dbfe60325948xxxxxxxxxx' # Found on Twilio Console Dashboard
-auth_token = 'e8043b9284969caxxxxxxxxxxxxxx' # Found on Twilio Console Dashboard
+#Twilio Setup
+account_sid = 'xxxxxx' # Found on Twilio Console Dashboard
+auth_token = 'xxxxxxx' # Found on Twilio Console Dashboard
+myPhone = '+4917xxxxxxx' # Phone number you used to verify your Twilio account
+TwilioNumber = '+1xxxxxxxxxxxx' # Phone number given to you by Twilio
+cities = ['BERLIN', 'HAMBURG', 'FRANKFURT', 'DRESDEN', 'ROSTOCK', 'GELSENKIRCHEN', 'BARCELONA', 'COPENHAGEN', 'ROTTERDAM', 'PARIS']
 
-myPhone = '+49172xxxxxxx' # Phone number you used to verify your Twilio account
-TwilioNumber = '+1xxxxxxxxx' # Phone number given to you by Twilio
-
-client = Client(account_sid, auth_token)
-
-url = 'https://www.fansale.de/fansale/tickets/hard-heavy/ozzy-osbourne/478'
-response = requests.get(url)
-soup = BeautifulSoup(response.content.decode('utf-8', 'ignore'), 'html.parser', )
-cities = ['BERLIN']
-
-data = soup.find_all("li", {'class':'Dropdown-ValuesEntry'})
-for x in data: 
-    y = x.string.strip().encode('utf-8')
-    if y in cities:
-        print ("Found new city "+ y) 
-        client.messages.create(
-  			to=myPhone,
- 			from_=TwilioNumber,
-  			body='I sent a text message from Python! ' + u'\U0001f680')
-
+while True:
+    #url = 'https://www.fansale.de/fansale/tickets/hard-heavy/ozzy-osbourne/478'
+    url = 'https://www.fansale.de/fansale/tickets/alle/rammstein/526'
+    client = Client(account_sid, auth_token)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content.decode('utf-8', 'ignore'), 'html.parser', )
+    data = soup.find_all("li", {'class':'Dropdown-ValuesEntry'})
+    for x in data: 
+        y = x.string.strip().encode('utf-8')
+        if y in cities:
+            print ("Found new city "+ y) 
+            client.messages.create(to=myPhone, from_=TwilioNumber,
+               body='Good News: Rammstein Tix available in ' + y+  u'\U0001f680')
+        else:
+            print ("Found nothing new") 
+    time.sleep(60)
